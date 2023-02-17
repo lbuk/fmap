7573ac62693be5c0df8672a5511aa81cef55a6ac
Liam Bolton <liamthomasbolton@gmail.com>
2023-02-17 19:19:40 +0000

    updates to documentation and README file

--- R/fmap_data.R
+++ R/fmap_data.R
@@ -12,7 +12,7 @@
 #' @param sum Variable from geo_points for calculating sum
 #' @param mean Variable from geo_pointsfor calculating mean
 #' @param median Variable from geo_points for calculating median
-#' @param count Count the number of points. Input TRUE to count points. Defaults to FALSE
+#' @param count Count the number of points from geo_points. Input TRUE to count points. Defaults to FALSE
 #' @return An sf dataset of Fresnel Map polygons based on aggregations of points-based data with a custom Azimuthal Equidistant (AEQD) CRS.
 #' @examples
 #' # Load the sf dataset of Soho pumps
